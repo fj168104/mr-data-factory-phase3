e272bda4fefb57a60aa811e450e38d6f3542fd5e
spacegithub <space_star@yeah.net>
2018-08-23 16:22:49 +0800

    增加opencv的本地库加载

--- src/main/java/com/mr/RootApplication.java
+++ src/main/java/com/mr/RootApplication.java
@@ -1,10 +1,15 @@
 package com.mr;
 
+import com.mr.common.OCRUtil;
 import com.mr.common.base.mapper.BaseMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.io.File;
 
 /**
  *
@@ -14,9 +19,24 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement // 开启注解事务管理，等同于xml配置文件中的 <tx:annotation-driven />
 @MapperScan(basePackages = "com.mr.*", markerInterface = BaseMapper.class)
 //@EnableAspectJAutoProxy(proxyTargetClass = true)
+@Slf4j
 public class RootApplication {
-    
-    public static void main(String[] args) {
-        SpringApplication.run(RootApplication.class, args);
+   public static void main(String[] args) {
+
+       SpringApplication.run(RootApplication.class, args);
+
+       String systemType = System.getProperty("os.name");
+
+       log.info("程序加载···opencv···中！");
+       /**
+        * 部署程序，需要将opencv_java341.dll或者os放到相应目录：DOWNLOAD_DIR
+        */
+       if(systemType.toLowerCase().startsWith("win")){
+           System.load(OCRUtil.DOWNLOAD_DIR+File.separator+"opencv_java341.dll");
+       }else {
+           System.load(OCRUtil.DOWNLOAD_DIR+File.separator+"opencv_java341.os");
+       }
+       log.info("程序加载···opencv···完成！");
+       log.info("****************************程序运行在{} 上*****************************",systemType);
     }
 }
