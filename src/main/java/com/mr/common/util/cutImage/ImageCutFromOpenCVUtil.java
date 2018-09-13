032016126bfbcb74777248185bbea44a4747c41d
spacegithub <space_star@yeah.net>
2018-09-13 12:02:53 +0800

    增加opencv的本地库只适用window系统

--- src/main/java/com/mr/common/util/cutImage/ImageCutFromOpenCVUtil.java
+++ src/main/java/com/mr/common/util/cutImage/ImageCutFromOpenCVUtil.java
@@ -77,13 +77,14 @@ public class ImageCutFromOpenCVUtil {
         StringBuffer stringBuffer = new StringBuffer("");
         List<String> listFileName = imgCut(basePath,fileTempSourceName, fileTempPath);
         for(String strFileName :listFileName){
-            stringBuffer.append(BaiduOCRUtil.getTextStrFromImageFile(strFileName,"")).append(",");
+            stringBuffer.append(AIOCRUtil.getTextFromImageFile(strFileName)).append(",");
+
         }
         log.info("表格识别结果为：{}",stringBuffer.toString());
 
         if(stringBuffer.toString().trim().equals("")){
             for(String strFileName :listFileName){
-                stringBuffer.append(AIOCRUtil.getTextFromImageFile(strFileName)).append(",");
+                stringBuffer.append(BaiduOCRUtil.getTextStrFromImageFile(strFileName,"")).append(",");
             }
         }
         text = stringBuffer.toString();
@@ -93,7 +94,7 @@ public class ImageCutFromOpenCVUtil {
             fileDel.delete();
         }
 
-        return text.replaceAll("（","(").replaceAll("）",")").replaceAll("[\\s]{1,}","");
+        return text.replaceAll("（","(").replaceAll("）",")").replaceAll("[\\s]{1,}","").replaceAll("[\\,]{2,}",",");
     }
 
     /**
